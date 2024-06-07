0ff68527d5237512348960396b3e881f19fcf030
Marcelo da Silva <marceloxhenrique@hotmail.com>
2024-06-07 14:14:50 +0200

    feat: Add Home Page and HowToGetThere Page. - Implements Home page with hero section, countdown clock, and menu for navigation. - Implements HowToGetThere page with info and links how to get there to the wedding location - Implements Menu and WeddingPlan page

---
+++ main.js
@@ -0,0 +1,24 @@
+const weddingDate = new Date("Jul 6, 2024 15:30:00").getTime();
+
+let interval = setInterval(() => {
+  const now = new Date().getTime();
+  const duration = weddingDate - now;
+  if (duration < 0) {
+    clearInterval(interval);
+    countDown(0);
+    return;
+  }
+  countDown(duration);
+}, 1000);
+
+const days = document.querySelector("#days-left");
+const hours = document.querySelector("#hours-left");
+const minutes = document.querySelector("#minutes-left");
+const seconds = document.querySelector("#seconds-left");
+
+const countDown = (duration) => {
+  days.innerText = Math.floor(duration / (1000 * 60 * 60 * 24));
+  hours.innerText = Math.floor((duration % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)) + 2;
+  minutes.innerText = Math.floor((duration % (1000 * 60 * 60)) / (1000 * 60));
+  seconds.innerText = Math.floor((duration % (1000 * 60)) / 1000);
+};
